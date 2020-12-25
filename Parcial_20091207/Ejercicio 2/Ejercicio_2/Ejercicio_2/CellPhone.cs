150427a1bc63424d5cdcce09da03a5dbc59efe87
Matias <mnfredes@gmail.com>
2020-12-25 11:15:43 -0300

    Agrego ejercicio 2

---
+++ Parcial_20091207/Ejercicio 2/Ejercicio_2/Ejercicio_2/CellPhone.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_2
+{
+    public abstract class CellPhone
+    {
+        private string number;
+        private bool isEnabled;
+
+        protected CellPhone(string number)
+        {
+            this.number = number;
+        }
+
+        public void Enable()
+        {
+            isEnabled = true;
+        }
+
+        public void Disable()
+        {
+            isEnabled = false;
+        }
+
+        public void MakeCall(string number)
+        {
+            if (!isEnabled)
+            {
+                throw new DisableCellPhoneException();
+            }
+
+            OnCall(number);
+        }
+
+        public string GetNumber()
+        {
+            return number;
+        }
+
+        protected abstract void OnCall(string number);
+
+        public override string ToString()
+        {
+            return number;
+        }
+
+        public override int GetHashCode()
+        {
+            return number.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null || !(obj is CellPhone))
+            {
+                return false;
+            }
+            return number.Equals(((CellPhone)obj).number);
+        }
+    }
+}
---
+++ Parcial_20091207/Ejercicio 2/Ejercicio_2/Ejercicio_2/DisableCellPhoneException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Ejercicio_2
+{
+    [Serializable]
+    internal class DisableCellPhoneException : Exception
+    {
+        public DisableCellPhoneException()
+        {
+        }
+
+        public DisableCellPhoneException(string message) : base(message)
+        {
+        }
+
+        public DisableCellPhoneException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected DisableCellPhoneException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
