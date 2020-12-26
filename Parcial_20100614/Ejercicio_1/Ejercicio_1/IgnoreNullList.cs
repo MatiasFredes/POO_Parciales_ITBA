fe2c6c676a44c58c6185b746f462391aaf01ddf6
Matias <mnfredes@gmail.com>
2020-12-26 18:44:28 -0300

    Resulevo ejercicio 1

---
+++ Parcial_20100614/Ejercicio_1/Ejercicio_1/IgnoreNullList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_1
+{
+    public class IgnoreNullList<T> : List<T>
+    {
+        private IEnumerable<T> GetValues()
+        {
+            foreach (var item in this)
+            {
+                if(item != null)
+                {
+                    yield return item;
+                }
+            }
+        }
+        public IEnumerator<T> IgnoreNullsIterator()
+        {
+            return this.GetValues().GetEnumerator();
+        }
+
+       
+    }
+}
---
+++ Parcial_20100614/Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new IgnoreNullList<int?>();
+
+            list.Add(1);
+            list.Add(null);
+            list.Add(2);
+            list.Add(5);
+            list.Add(null);
+            list.Add(3);
+            list.Add(null);
+
+            IEnumerator<int?> enumerator = list.IgnoreNullsIterator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
